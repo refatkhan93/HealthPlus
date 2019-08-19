db31bd01703e6008df515a34288aab61b1e77a81
refatkhan <refatkhan93@gmail.com>
2019-08-20 01:34:44 +0600

    1st commit

---
+++ HealthPlus/Controllers/PrimaryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HealthPlus.Controllers
+{
+    public class PrimaryController : Controller
+    {
+        //
+        // GET: /Primary/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ HealthPlus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HealthPlus.Startup))]
+namespace HealthPlus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
